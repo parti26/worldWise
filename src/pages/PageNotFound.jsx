d772fe3ebbe240d7bf6c526b97d07433ebbe76f6
parti26 <partosreka26@gmail.com>
2024-10-02 21:21:00 +0200

    created PageNav jsx, which is a reusable component for navigation that can be used in all pages

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,13 @@
+import PageNav from "../components/PageNav";
+
 function PageNotFound() {
-  return <div>The page you are looking for does not exist.</div>;
+  return (
+    <div>
+      {" "}
+      <PageNav />
+      The page you are looking for does not exist.
+    </div>
+  );
 }
 
 export default PageNotFound;
