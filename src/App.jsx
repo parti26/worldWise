e6da3bdaa3c39a3af5ed47388212589784caf7fb
parti26 <partosreka26@gmail.com>
2024-10-07 17:42:40 +0200

    implemented sub navigation for the app component

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          /** nested routes - child routes of app element */
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>list of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
